64f2056f0db9bf7ebf16b33d223aeec7873c821e
yujinkim1 <yujin7621@me.com>
2023-08-15 21:25:17 +0900

    [delete] gitkeep 파일

--- NBCamp/Utility/ColorManager.swift
+++ NBCamp/Utility/ColorManager.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 struct ColorManager {
-	
 	static let nbcPrimaryColor = UIColor(named: "nbcPrimaryColor")
 	static let nbcSecondaryColor = UIColor(named: "nbcSecondaryColor")
 	static let nbcTertiaryColor = UIColor(named: "nbcTertiaryColor")
 	static let nbcAccentColor = UIColor(named: "nbcAccentColor")
 	static let nbcBackgroundColor = UIColor(named: "nbcBackgroundColor")
 	static let nbcTintColor = UIColor(named: "nbcTintColor")
-	
 }
