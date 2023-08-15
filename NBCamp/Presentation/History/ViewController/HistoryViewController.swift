f47ddcbc282e8f8d24c409eab0e5364ffc2dfc81
yujinkim1 <yujin7621@me.com>
2023-08-16 00:11:41 +0900

    [modi] #8 스토리보드 레이아웃

--- NBCamp/Presentation/History/ViewController/HistoryViewController.swift
+++ NBCamp/Presentation/History/ViewController/HistoryViewController.swift
@@ -5,25 +5,20 @@
 //  Created by Yujin Kim on 2023-08-14.
 //
 
+import SafariServices
 import UIKit
 
 class HistoryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = ColorManager.nbcBackgroundColor
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+	}
+	
+	override func viewDidDisappear(_ animated: Bool) {
+		super.viewDidDisappear(animated)
+	}
 }
