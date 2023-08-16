73399942cb354119ca8cf4f9ecf1dbe62abd267d
“hw3590” <lkhs44@naver.com>
2023-08-16 09:45:44 +0900

    [add] "테이블뷰 추가"

--- NBCamp/Presentation/Community/ViewController/CommunityViewController.swift
+++ NBCamp/Presentation/Community/ViewController/CommunityViewController.swift
@@ -8,14 +8,45 @@
 import UIKit
 
 class CommunityViewController: UIViewController {
+    
+    let formatter: DateFormatter = {
+           let f = DateFormatter()
+           f.dateStyle = .long
+           f.timeStyle = .short
+           return f
+       }()
+       
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		setTitle()
+        setTitle()
     }
-	
-	private func setTitle() {
-		title = "커뮤니티"
-	}
-
+    
+    private func setTitle() {
+        title = "커뮤니티"
+    }
 }
+
+extension CommunityViewController: UITableViewDataSource, UITableViewDelegate {
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Board.dummyBoardList.count
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let boardcell = tableView.dequeueReusableCell(withIdentifier: "boardcell", for: indexPath)
+              
+              let target = Board.dummyBoardList[indexPath.row]
+              boardcell.textLabel?.text = target.content
+        boardcell.detailTextLabel?.text = formatter.string(from: target.insertDate)
+              
+              return boardcell
+    }
+}
+
+
+
+
+
