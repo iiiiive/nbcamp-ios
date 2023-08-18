a4a5eff5f1e02c1d6c78afcb41672124c25b2ef5
yujinkim1 <yujin7621@me.com>
2023-08-19 01:32:00 +0900

    [add] 사용자 정보 수정 스토리보드

---
+++ NBCamp/Presentation/Edit/ViewController/EditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EditViewController.swift
+//  NBCamp
+//
+//  Created by Yujin Kim on 2023-08-19.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+}
