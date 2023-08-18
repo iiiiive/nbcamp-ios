7aa58fc85ca3f883e97fb41f6c35c5e60861e407
yujinkim1 <yujin7621@me.com>
2023-08-19 01:29:31 +0900

    [feat] 외출 버튼 활성화

--- NBCamp/Presentation/Profile/ViewController/ProfileViewController.swift
+++ NBCamp/Presentation/Profile/ViewController/ProfileViewController.swift
@@ -27,6 +27,7 @@ class ProfileViewController: UIViewController {
 				timer.invalidate()
 				self.timer = nil
 				sender.setTitle("입실", for: .normal)
+				self.timerLabel.text = "00:00:00"
 			}))
 			present(alert, animated: true, completion: nil)
 		} else {
@@ -37,18 +38,41 @@ class ProfileViewController: UIViewController {
 		}
 	}
 	@IBAction func pause(_ sender: UIButton) {
-		
+		if let timer = timer {
+			let alert = UIAlertController(title: "외출 하시겠습니까?", message: nil, preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+			alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { (_) in
+				timer.invalidate()
+				self.timer = nil
+				self.timerLabel.textColor = ColorManager.nbcTertiaryColor
+				sender.setTitle("복귀", for: .normal)
+			}))
+			present(alert, animated: true, completion: nil)
+		}
+		if sender.titleLabel?.text == "복귀" {
+			timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeLabel), userInfo: nil, repeats: true)
+			self.timerLabel.textColor = ColorManager.nbcAccentColor
+			sender.setTitle("외출", for: .normal)
+			startTimeLabel.text = DateTimeFormatter.createDateTime()
+			attendanceTimeLabel.text = DateTimeFormatter.createDateTime()
+		}
 	}
-	
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
         setUsername()
+		setTimerLabel()
     }
 	
 	private func setUsername() {
 		let user = User()
 		usernameLabel.text = user.username + "님, 오늘도 달려볼까요?"
+	}
+	
+	private func setTimerLabel() {
+		timerLabel.layer.cornerRadius = CGFloat(10.0)
+		timerLabel.layer.borderColor = ColorManager.nbcAccentColor?.cgColor
+		timerLabel.layer.borderWidth = CGFloat(1.0)
 	}
 	
 	@objc func updateTimeLabel() {
