2d7827736e21cd7992c5c9d1256ced29fa35261d
yujinkim1 <yujin7621@me.com>
2023-08-20 14:30:09 +0900

    [feat] 게시글 상세 화면

--- NBCamp/Presentation/Detail/ViewController/DetailViewController.swift
+++ NBCamp/Presentation/Detail/ViewController/DetailViewController.swift
@@ -12,12 +12,43 @@ class DetailViewController: UIViewController {
 	@IBOutlet weak var datetimeLabel: UILabel!
 	@IBOutlet weak var postImageView: UIImageView!
 	@IBOutlet weak var contentLabel: UITextView!
+	@IBOutlet weak var thumbView: UIView!
+	@IBOutlet weak var thumbDownButton: UIButton!
+	@IBOutlet weak var thumbUpButton: UIButton!
 	
-	var post: Post
+	@IBAction func selectDown(_ sender: UIButton) {
+		thumbDownButton.isSelected.toggle()
+		if thumbDownButton.isSelected {
+			thumbDownButton.tintColor = ColorManager.nbcAccentColor
+			thumbDownButton.setTitleColor(ColorManager.nbcAccentColor, for: .selected)
+			thumbDownButton.setImage(UIImage(systemName: "hand.thumbsdown.fill"), for: .selected)
+		} else {
+			thumbDownButton.tintColor = ColorManager.nbcTertiaryColor
+			thumbDownButton.setTitleColor(ColorManager.nbcPrimaryColor, for: .normal)
+			thumbDownButton.setImage(UIImage(systemName: "hand.thumbsdown"), for: .normal)
+		}
+	}
+	
+	@IBAction func selectUp(_ sender: UIButton) {
+		thumbUpButton.isSelected.toggle()
+		if thumbUpButton.isSelected {
+			thumbUpButton.tintColor = ColorManager.nbcAccentColor
+			thumbUpButton.setTitleColor(ColorManager.nbcAccentColor, for: .selected)
+			thumbUpButton.setImage(UIImage(systemName: "hand.thumbsup.fill"), for: .selected)
+		} else {
+			thumbUpButton.tintColor = ColorManager.nbcTertiaryColor
+			thumbUpButton.setTitleColor(ColorManager.nbcPrimaryColor, for: .normal)
+			thumbUpButton.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
+		}
+	}
+	
+	var post: Post?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		DetailViewDecorator.run(detailViewController: self, selectedPost: post)
+		if let selectedPost = post {
+			DetailViewDecorator.run(detailViewController: self, selectedPost: selectedPost)
+		}
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -26,10 +57,6 @@ class DetailViewController: UIViewController {
 	
 	override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
-	}
-	
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
 	}
 }
 //MARK: 게시글 상세 뷰 데코레이터
@@ -46,7 +73,7 @@ class DetailViewDecorator {
 	}
 	
 	private static func decorateDateTimeLabel(_ detailViewController: DetailViewController, _ selectedPost: Post) {
-		detailViewController.datetimeLabel.text = selectedPost.date
+		detailViewController.datetimeLabel.text = selectedPost.username + selectedPost.date
 	}
 	
 	private static func decoratePostImageView(_ detailViewController: DetailViewController, _ selectedPost: Post) {
